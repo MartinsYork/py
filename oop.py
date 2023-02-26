1aff464bce46e2757e9996344c80597edc661c4c
MartinsYork <martinskingzion@gmail.com>
2023-02-26 03:47:56 +0100

    Added the call method

--- oop.py
+++ oop.py
@@ -3,3 +3,10 @@ class Customer:
         self.name = name
         self.membership_type = membership_type
         print("Customer created")
+
+c = Customer("York", "Diamond")
+print(c.name, c.membership_type)
+
+c2 = Customer("marino", "Danger")
+print(c2.name, c2.membership_type)
+
